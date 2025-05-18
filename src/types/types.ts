55d359088497bd7002fd12f6bf5971e219cf4da5
Fabrizzioxc <fabrizziozv.17@gmail.com>
2025-05-18 00:27:44 -0500

    refactor: Define el tipo Cliente en types.ts para mejorar la estructura de datos

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,3 +7,13 @@ export type Categoria = {
   estado: "A" | "I";
   categoria_padre_id: string | null;
 };
+export type Cliente = {
+  id: string;
+  nombres: string;
+  apellido_paterno: string;
+  apellido_materno: string;
+  celular: string;
+  email: string;
+  estado: "A" | "I";
+  created_at: string;
+};
